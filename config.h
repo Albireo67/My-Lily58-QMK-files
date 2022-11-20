fdce99fa630bc82ee0f4390875f40298335eb52c
Albireo67 <Albireo67@users.noreply.github.com>
2022-11-20 21:08:30 +0100

    Update config.h forcing N-Key Rollover

--- config.h
+++ config.h
@@ -26,6 +26,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //define MASTER_RIGHT
 // #define EE_HANDS
 
+//N-Key Rollover may be turned on here
+#define FORCE_NKRO
+
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
 
